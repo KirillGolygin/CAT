5c2fb230ff79ead87272dc9621575eb608d02b10
Kirill Golygin <k.golygin.dev@gmail.com>
2023-12-19 22:43:37 +0300

    add structure and default styles

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
       {
         test: /\.(css|scss)$/,
         exclude: /node_modules/,
-        use: ["style-loader", "css-loader"]
+        use: ["style-loader", "css-loader", 'sass-loader']
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
