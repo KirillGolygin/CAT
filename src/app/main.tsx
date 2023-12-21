5749d217beb0cf95f7737c119fdb9a6d2cf2ce8c
Kirill Golygin <k.golygin.dev@gmail.com>
2023-12-21 14:48:08 +0300

    move colors and shadow to variables

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
 import App from './App';
 import './styles/main.scss';
+import './styles/variables.scss';
 import ErrorBoundaryComponent from './system/ErrorBoundary/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
