5848c21acf34cf46a3afdc1894b0fbdc9d094ea9
Kirill Golygin <k.golygin.dev@gmail.com>
2023-12-21 13:02:00 +0300

    create home page

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+
+import CurrencyStore from 'src/shared/store/currency-store';
+
+import { Logo, Selector } from 'src/shared/components';
+
+import './HomePage.scss';
+
+const HomePage = () => {
+  const { getCurrencies, pickedCurrency, errorMessage, currencies, pickCurrency } = CurrencyStore();
+  const [isSelectorOpen, setIsSelectorOpen] = useState<boolean>(false);
+
+  const closeSelector = () => {
+    setIsSelectorOpen(false);
+  };
+
+  const pickCur = (id: string) => {
+    pickCurrency(id);
+    setIsSelectorOpen(false);
+  };
+
+  useEffect(() => {
+    getCurrencies();
+  }, [getCurrencies]);
+
+  return (
+    <section className="container" onClick={closeSelector}>
+      <div className="main-container">
+        <div className="logo-container">
+          <Logo />
+          <Selector
+            items={currencies}
+            pickItem={(id) => pickCur(id)}
+            isOpen={isSelectorOpen}
+            setIsOpen={setIsSelectorOpen}
+            pickedItem={pickedCurrency}
+          />
+        </div>
+        <div className="pic-container">
+          <img src="/Kitten.png" />
+        </div>
+      </div>
+      <div className="currency-name-container">
+        <p className="currency-name"> {errorMessage ? errorMessage : pickedCurrency?.name}</p>
+      </div>
+    </section>
+  );
+};
+
+export default HomePage;
